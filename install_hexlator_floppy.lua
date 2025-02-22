f06e88cdbb5940d0009fe1e7a0bcdeb0fecad5e5
Vizoee <193793023+Vizoee@users.noreply.github.com>
2025-02-22 16:08:22 +0100

    fast fix to floppy installer :3

--- install_hexlator_floppy.lua
+++ install_hexlator_floppy.lua
@@ -18,10 +18,12 @@ else
 end
 
 local install_path_symbol
-if not args[3] or args[3] == false then
-    install_path_symbol = install_path
-else
+if not args[3] or args[3] == true then
     install_path_symbol = "/programfiles/hexlator/"
+end
+
+else
+    install_path_symbol = install_path
 end
 
 shell.execute("delete", install_path)
